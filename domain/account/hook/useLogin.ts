b8928f4e4668c7b6d5218ad18bee56386ef94935
ChandraRaditya <chandraariwiraditya@gmail.com>
2024-08-29 23:21:48 +0700

    refactor(haip-28): change icon props and refactor uselogin hook

--- domain/account/hook/useLogin.ts
+++ domain/account/hook/useLogin.ts
@@ -1,9 +1,10 @@
-import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { cookie } from "@/domain/shared/utils";
 import { CustomApiServiceError } from "@/domain/shared/services";
 import { getBaseData } from "@/domain/shared/mappers";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { login } from "../services/api";
 import { getLoginData } from "../mappers";
 
@@ -15,17 +16,13 @@ export const loginSchema = z.object({
 export type TFormLogin = z.infer<typeof loginSchema>;
 
 export const useLogin = () => {
-  const [emailError, setEmailError] = useState<string>("");
-  const [passwordError, setPasswordError] = useState<string>("");
-  const { register, handleSubmit } = useForm<TFormLogin>();
-
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const formMethod = useForm<TFormLogin>({
+    resolver: zodResolver(loginSchema),
+    mode: "onSubmit",
+  });
+  const { setError } = formMethod;
   const handleLogin: SubmitHandler<TFormLogin> = async (data) => {
-    const resultLogin = loginSchema.safeParse(data);
-    if (!resultLogin.success) {
-      setEmailError(resultLogin.error?.format().email?._errors.join("") || "");
-      setPasswordError(resultLogin.error?.format().password?._errors.join("") || "");
-    }
-
     try {
       const response = await login({
         email: data.email,
@@ -36,8 +33,10 @@ export const useLogin = () => {
       const baseData = getBaseData(response);
 
       if (baseData.errorCode === 1001) {
-        setEmailError("oops, email atau password salah.");
-        setPasswordError("oops, email atau password salah.");
+        setError("email", { message: "oops, email atau password salah." });
+        setError("password", {
+          message: "oops, email atau password salah.",
+        });
         throw new CustomApiServiceError(
           `error code ${baseData.errorCode}, status code ${baseData.statusCode}, message ${baseData.message} on useLogin level`,
           "Error",
@@ -51,7 +50,7 @@ export const useLogin = () => {
     }
   };
 
-  return { handleLogin, register, handleSubmit, emailError, passwordError };
+  return { handleLogin, formMethod, isShowPassword, setIsShowPassword };
 };
 
 export default useLogin;
--- domain/account/ui/LoginForm/LoginForm.tsx
+++ domain/account/ui/LoginForm/LoginForm.tsx
@@ -1,29 +1,46 @@
 "use client";
 
 import { Button, TextInput } from "@/domain/shared/ui";
-import Haipiy from "@/domain/shared/assets/svg/haipiy.svg";
+import LgHaipiy from "@/domain/shared/assets/svg/haipiy.svg";
 import { useLogin } from "@/domain/account/hook";
+import IcVisibility from "@mui/icons-material/Visibility";
 
 export const LoginForm = () => {
-  const { handleLogin, handleSubmit, register, emailError, passwordError } = useLogin();
+  const { handleLogin, formMethod, isShowPassword, setIsShowPassword } = useLogin();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = formMethod;
   return (
     <div className="flex min-h-[369px] w-full max-w-[406px] flex-col gap-4">
       <div className="flex flex-col gap-4 rounded-[10px] bg-haip-black-100 p-[18px]">
         <form onSubmit={handleSubmit(handleLogin)} className="flex flex-col">
           <h2 className="mb-4 text-[32px] font-normal leading-[38px]">Masuk</h2>
           <TextInput
-            {...register("email")}
+            {...register("email", {
+              required: "wajib diisi",
+            })}
+            required
             label="Email"
             placeholder="Masukan email kamu"
-            errorMessage={emailError}
-            required
+            errorMessage={errors.email?.message}
           />
           <TextInput
-            {...register("password")}
+            {...register("password", {
+              required: "wajib diisi",
+            })}
+            required
             label="Password"
             placeholder="Masukan password kamu"
-            errorMessage={passwordError}
-            required
+            showPassword={!!isShowPassword}
+            errorMessage={errors.password?.message}
+            iconEnd={
+              <IcVisibility
+                className="cursor-pointer"
+                onClick={() => setIsShowPassword(!isShowPassword)}
+              />
+            }
           />
           <Button className="mt-4 w-full" type="submit">
             Login
@@ -33,7 +50,7 @@ export const LoginForm = () => {
           Register
         </Button>
       </div>
-      <Haipiy className="h-[45.68] w-[124px] self-center" />
+      <LgHaipiy className="h-[45.68] w-[124px] self-center" />
     </div>
   );
 };
