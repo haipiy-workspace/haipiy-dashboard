8629e76a732d9eade60d4a74280f9c9d57f9e343
ChandraRaditya <chandraariwiraditya@gmail.com>
2024-09-11 21:43:13 +0700

    refactor(haip-18): refactor checkbox value

--- domain/shared/ui/Checkbox/Checbox.tsx
+++ domain/shared/ui/Checkbox/Checbox.tsx
@@ -8,25 +8,24 @@ interface ICheckboxProps extends ComponentPropsWithoutRef<"input"> {
   className?: string;
   checked?: boolean;
   value?: string;
-  id?: string;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const Checkbox = forwardRef<HTMLInputElement, ICheckboxProps>(
-  ({ sizeCheckbox = "medium", label, className, checked, value, onChange, id, ...props }, ref) => (
-    <label className="flex flex-row items-center gap-2" htmlFor={id}>
-        <MCheckbox
-          id={id}
-          className={className}
-          size={sizeCheckbox}
-          checked={checked}
-          value={value}
-          onChange={onChange}
-          inputRef={ref}
-          inputProps={{ ...props }}
-        />
-        {label}
-      </label>
+  ({ sizeCheckbox = "medium", label, className, checked, onChange, value, ...props }, ref) => (
+    <label className="flex flex-row items-center gap-2" htmlFor={value}>
+      <MCheckbox
+        id={value}
+        value={value}
+        className={className}
+        size={sizeCheckbox}
+        checked={checked}
+        onChange={onChange}
+        inputRef={ref}
+        inputProps={{ ...props }}
+      />
+      {label}
+    </label>
   ),
 );
 
